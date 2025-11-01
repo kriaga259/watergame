8dc2092728fe6f957ba604c30de99ca3204dc921
kriaga259 <akrisha259@gmail.com>
2025-10-31 23:26:36 -0700

    update game

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,11 +1,16 @@
 extends Node
 
+var total_coins: int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func coin_collected(value: int):
+	total_coins += value
+	EventController.emit_signal("coin_collected", total_coins)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
